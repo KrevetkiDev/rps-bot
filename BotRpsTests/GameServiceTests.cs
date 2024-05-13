bfb92641bf32f66800ee3d94fa1c3a2e3afe3587
Diana Agapova <d18102002@mail.ru>
2024-05-13 18:11:20 +0300

    add args for tests

--- BotRpsTests/GameServiceTests.cs
+++ BotRpsTests/GameServiceTests.cs
@@ -2,14 +2,20 @@ using BotRpc.Domain.Enums;
 using BotRps.Infrastructure.Services;
 using FluentAssertions;
 
-namespace tests;
+namespace BotRps.Infrastructure.Tests;
 
 public class GameServiceTests
 {
     [Theory]
     [InlineData(RpsItems.Rock, RpsItems.Scissors, GameResultTypes.PlayerWin)]
     [InlineData(RpsItems.Rock, RpsItems.Paper, GameResultTypes.BotWin)]
+    [InlineData(RpsItems.Rock, RpsItems.Rock, GameResultTypes.Draw)]
     [InlineData(RpsItems.Scissors, RpsItems.Scissors, GameResultTypes.Draw)]
+    [InlineData(RpsItems.Scissors, RpsItems.Paper, GameResultTypes.PlayerWin)]
+    [InlineData(RpsItems.Scissors, RpsItems.Rock, GameResultTypes.BotWin)]
+    [InlineData(RpsItems.Paper, RpsItems.Paper, GameResultTypes.Draw)]
+    [InlineData(RpsItems.Paper, RpsItems.Rock, GameResultTypes.PlayerWin)]
+    [InlineData(RpsItems.Paper, RpsItems.Scissors, GameResultTypes.BotWin)]
     public void GameService_ShouldReturnResult_ValidArgs(RpsItems playerChoice, RpsItems botChoice,
         GameResultTypes expectedResult)
     {
